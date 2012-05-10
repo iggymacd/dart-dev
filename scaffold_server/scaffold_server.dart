2d756888c18b98f6e78082cb0236a4a56790e11a
unknown <al@al-PC.(none)>
2012-05-10 16:06:20 -0300

    added some comments in server code

--- scaffold_server/scaffold_server.dart
+++ scaffold_server/scaffold_server.dart
@@ -3,12 +3,15 @@
 
 void main() {
   HttpServer server = new HttpServer();
-  
+  //define what will happen if there are no matches for a handler
   server.defaultRequestHandler = (req,resp) => 
       new DefaultHandler().onRequest(req,resp);
+  //define where the favicon.ico can be found
   server.addRequestHandler((req) => 
       req.path == '/favicon.ico', (req,resp) => 
           new FileHandler().onRequest(req,resp,'static/images/favicon.ico'));
+  //direct all other calls to a client side handler to serve up files from a 
+  //dart web client project folder
   server.addRequestHandler((req) => 
       req.path.startsWith('/') && req.method == 'GET', (req,resp) => 
           new ClientSideHandler().onRequest(req,resp,'../scaffold_client'));
